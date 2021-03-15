ddf750ee4d661014c048d61b78f83163c8b92538
KrystianOg <62177596+KrystianOg@users.noreply.github.com>
2021-03-15 02:22:57 +0100

    added some functionality to Animals Panel

--- CS/IFeedAnimal.cs
+++ CS/IFeedAnimal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Zoo_Management.CS
 {
@@ -10,7 +11,7 @@ namespace Zoo_Management.CS
     {
         DateTime LastFeedTime { get; set; }
 
-        void Feed(); 
+        void Feed(object sender, RoutedEventArgs e); 
     }
 
 }
